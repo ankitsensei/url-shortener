51a8a31d9e5e8599590d3f59656318751b46399b
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-05-04 15:21:50 +0530

    feat: visitHistory (number of website opened with that short url) will be saved in db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import urlRoute from "./routes/url.js";
 import { connectToMongoDB } from "./connect.js";
+import URL from "./models/url.js";
 import { configDotenv } from "dotenv";
 
 configDotenv();
@@ -16,6 +17,21 @@ connectToMongoDB("mongodb://127.0.0.1:27017/url-shortener ").then(() =>
 app.use(express.json());
 
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitedHistory: { timestamp: Date.now() },
+      },
+    },
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => {
   console.log("App is running on port:", PORT);
