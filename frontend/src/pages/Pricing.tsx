a371dab4665e2e5c06dffff427c27ea9c218f470
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-05-12 23:00:23 +0530

    feat: Pricing page basic layout

--- frontend/src/pages/Pricing.tsx
+++ frontend/src/pages/Pricing.tsx
@@ -2,8 +2,11 @@ import Navbar from "../components/Navbar";
 
 const Pricing = () => {
   return (
-    <div className="w-full h-full bg-[#E3DEFF]">
+    <div className="w-full h-screen bg-[#E3DEFF]">
       <Navbar />
+      <div className="flex flex-col justify-center items-center h-1/2">
+        <p className="text-4xl text-purple-400">It's absolutely free</p>
+      </div>
     </div>
   );
 };
