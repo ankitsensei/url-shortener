582cee74d5e9500be344ef0b8c690dc3fc78e3f8
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-05-14 23:41:16 +0530

    feat: ui

--- frontend/src/pages/Hero.tsx
+++ frontend/src/pages/Hero.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { motion } from "motion/react";
 import toast, { Toaster } from "react-hot-toast";
 import { FaRegCopy } from "react-icons/fa";
+import { IoLinkSharp } from "react-icons/io5";
 
 interface IFormInput {
   newUrl: string;
@@ -46,21 +47,38 @@ const Hero = () => {
       <Toaster position="top-center" reverseOrder={false} />
       <Navbar />
       <div className="flex flex-col justify-center items-center mt-20">
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col items-center gap-5">
+          <div className="flex flex-col items-center">
+            <p className="font-bold text-5xl text-purple-950">Shorter Links.</p>
+            <p className="font-light text-6xl text-purple-900">
+              Deep Engagement
+            </p>
+          </div>
+          <p className="text-zinc-600 text-center">
+            Take full control. Create short, branded links and QR codes you can
+            edit
+            <br />
+            anytime to keep your campaigns fresh and effective.
+          </p>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-10">
           <label>Enter your URL</label>
-          <div>
-            <input
-              type="text"
-              placeholder="https://github.com/ankitsensei"
-              {...register("newUrl", { required: true })}
-              className="border border-r-0 py-2 px-2 w-110 outline-none rounded-lg rounded-r-none placeholder:text-zinc-400"
-            />
-            {errors.newUrl && <span>This field is required</span>}
+          <div className="flex items-center">
+            <div className="border border-r-0 w-110 outline-none rounded-lg rounded-r-none placeholder:text-zinc-400 flex items-center px-2">
+              <IoLinkSharp className="text-zinc-400 rotate-135 text-2xl" />
+              <input
+                type="text"
+                placeholder="https://github.com/ankitsensei"
+                {...register("newUrl", { required: true })}
+                className=" py-2 px-2 w-110 outline-none placeholder:text-zinc-400 flex"
+              />
+            </div>
             <input
               type="submit"
               className="px-5 py-2 bg-purple-400 border rounded-lg rounded-l-none"
             />
           </div>
+          {errors.newUrl && <span>This field is required</span>}
         </form>
         {/*TODO: Show link and copy button after submission of original link */}
         {generatedNewUrl && (
